c5a64cfbb5272271313e5b4d57042d9cde090312
LinFa <linfayang@live.com>
2018-04-06 12:16:29 +0800

    ShardPreference存储

---
+++ app/src/main/java/com/example/linfa/myshardpreference/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.linfa.myshardpreference;
+/**
+ * @date 2018/4/6
+ * @author skylinelin
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
